f3d9467a9be847905af04c975a847f55def25af9
Barbara Rodriguez <barbs2089@gmail.com>
2022-06-07 22:01:23 +1000

    Add event handler for search button to simulate a search query

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
         }
         this.handleTermChange = this.handleTermChange.bind(this)
         this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this);
 
         this.sortByOptions = {
             'best_match': 'Best Match',
@@ -39,6 +40,12 @@ class SearchBar extends React.Component {
         this.setState({ location: e.target.value});
     }
 
+    handleSearch(e) {
+        const { term, location, sortBy } = this.state;
+        this.props.searchYelp(term, location, sortBy);
+        e.preventDefault();
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             let sortByOptionValue = this.sortByOptions[sortByOption];
@@ -57,7 +64,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
             </div>
         )
